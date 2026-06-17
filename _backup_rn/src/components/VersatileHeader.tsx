8003c6521e5b5541d0984929ac956ea85b124834
nfernandez <nicolas.fernandezu@fortyone.com.ar>
2026-06-17 20:11:57 -0300

    Borrado del proyecto anterior

--- _backup_rn/src/components/VersatileHeader.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-// Este componente ha sido retirado a favor de las cabeceras de navegación nativas.
-// Retornamos null para evitar duplicados en las vistas y limpiar la pantalla por completo.
-const VersatileHeader = () => {
-  return null;
-};
-
-export default VersatileHeader;
